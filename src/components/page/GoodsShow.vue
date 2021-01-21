5015e03fe4fab6f7a13458d8083c0b997bb28982
qjc <15036750423@163.com>
2021-01-21 18:51:55 +0800

    商品前台

--- src/components/page/GoodsShow.vue
+++ src/components/page/GoodsShow.vue
@@ -27,7 +27,8 @@
           <el-table-column
             prop="brandid"
             label="品牌"
-            width="180">
+            width="180"
+            :formatter="brandshow">
           </el-table-column>
 
           <el-table-column
@@ -60,6 +61,7 @@
           <el-table-column
             prop="typeid"
             label="类型"
+            :formatter="proty"
             width="180">
           </el-table-column>
 
@@ -79,6 +81,14 @@
           </el-table-column>
 
         </el-table>
+        <el-pagination
+          @size-change="sc"
+          @current-change="cc"
+          :page-sizes="sizes"
+          :page-size="limit"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="count">
+        </el-pagination>
       </div>
     </div>
 </template>
@@ -88,17 +98,100 @@
         name: "GoodsShow",
       data(){
           return{
-            GoodsData:[]
+            sizes:[2,5,8],
+            limit:2,
+            count:0,
+            page:1,
+            GoodsData:[],
+            protypes:[],
+            protype:[],
+            brandData:[]
           }
       },created:function () {
-        this.chaGoods();
+        this.chaGoods(1);
+        this.chatype();
+        this.chaBrand();
       },methods:{
-        chaGoods:function () {
-          this.$axios.post("http://localhost:8080/api/type/chaGoods?page=1&limit=2").then(dd=>{
+        chaGoods:function (page) {
+          this.$axios.post("http://localhost:8080/api/type/chaGoods?page="+page+"&limit="+this.limit).then(dd=>{
             this.GoodsData=dd.data.data.data;
+            this.count=dd.data.data.count;
           }).catch(function () {
             alert("error")
           })
+        },
+        chatype:function () {
+          this.$axios.get("http://localhost:8080/api/type/getData").then(dd=>{
+            this.protype=dd.data.data;
+            this.getChildType();
+            for (let i = 0; i <this.protypes.length ; i++) {
+              this.proTypeName="";
+              this.pushptName(this.protypes[i]);
+              var qwerdf=this.proTypeName.split("/").reverse().join("/");
+              this.protypes[i].name=qwerdf.substr(0,qwerdf.length-1);
+            }
+          }).catch(function () {
+            alert("error")
+          })
+        },
+        pushptName:function(node){
+          if (node.id!=0){
+            this.proTypeName+="/"+node.name;
+            for (let i = 0; i <this.protype.length ; i++) {
+              if (node.pid==this.protype[i].id) {
+                this.pushptName(this.protype[i]);
+                break;
+              }
+            }
+          }else {
+            this.proTypeName+="/"+node.name;
+          }
+
+        },
+        getChildType:function(){
+          for (let i = 0; i <this.protype.length ; i++) {
+            var node=this.protype[i];
+            this.isChildNode(node)
+          }
+        },
+        isChildNode:function(node){
+          var tof=true;
+          for (let i = 0; i < this.protype.length; i++) {
+            if (node.id==this.protype[i].pid) {
+              tof=false;
+              break;
+            }
+          }
+          if (tof==true){
+            this.protypes.push(node);
+          }
+        },
+        proty:function (e,r) {
+          for (let i = 0; i <this.protype.length ; i++) {
+            if (e.typeid==this.protype[i].id) {
+              return this.protype[i].name;
+            }
+          }
+        },chaBrand:function (page) {
+          this.$axios.post("http://localhost:8080/api/type/chaBrand?page="+1+"&limit="+1000).then(dd=>{
+            this.brandData=dd.data.data.data;
+          }).catch(function () {
+            alert("erer")
+          })
+        },
+        brandshow:function (e,r) {
+          for (let i = 0; i <this.brandData.length ; i++) {
+            if (e.brandid==this.brandData[i].id){
+              return this.brandData[i].name;
+            }
+          }
+        },
+        sc:function (limit) {
+          this.limit=limit;
+          this.chaGoods(1);
+        },
+        cc:function (page) {
+          this.chaGoods(page);
         }
       }
     }
